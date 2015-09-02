100ea2d727e6db5a5340f6eea030f8be2a1f83c3
Vishvajit Pathak <vishvajit.pathak@incentius.com>
2015-09-02 19:08:39 +0530

    added normal function

--- decorators.py
+++ decorators.py
@@ -1,21 +1,6 @@
 # first define A normal function in python which returns something
-# def function_returns_something(arg1=4, arg2=5):
-#     return arg1+arg2
+def function_returns_something(arg1=4, arg2=5):
+    return arg1+arg2
 
-# function_returns_something(2,6)
-
-# a surprise : python function can return a function
-def function_returned(arg1="Hello"):
-    print '%s' % arg1
-
-def function_returns_another_function():
-    return function_returned
-    # print 'before calling returned function'
-    # funct('I have returned a function here !')
-    # print 'after calling returned function'
-
-call_me = function_returns_another_function()
-print call_me.__name__   # ouput : function_returned
-call_me('call me here !')
-
-# function_returns_another_function(function_returned)
+catch_value = function_returns_something(2,6)
+print catch_value
